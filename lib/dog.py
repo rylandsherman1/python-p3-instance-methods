af9ef3ad6a0f8b1fcad61125db49963688c878b8
Ryland Sherman <rylandsherman1@gmail.com>
2023-12-12 16:07:56 -0700

    complete assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
-class Dog:
-    # Class body goes here
 
-    #Instance method definition
-    pass
+class Dog:
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print("The dog is sitting.")
+
+
+# Creating an instance of Dog
+fido = Dog()
+fido.bark()
+fido.sit()  # Calling the new sit() method
--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
-class Person:
-    # Class body goes here
 
-    #Instance method definition
-    pass
+class Person:
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+
+
+# Creating an instance of Person
+john = Person()
+john.talk()  # Calling the talk() method
+john.walk()  # Calling the walk() method
